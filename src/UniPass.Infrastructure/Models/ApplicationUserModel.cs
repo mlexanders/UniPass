5d831d495ee99fefc91bc115ca372f23de0e16db
mlexanders <mlexanders@outlook.com>
2024-06-06 01:59:11 +0300

    ~

--- src/UniPass.Infrastructure/Models/ApplicationUserModel.cs
+++ src/UniPass.Infrastructure/Models/ApplicationUserModel.cs
@@ -5,12 +5,9 @@ namespace UniPass.Infrastructure.Models;
 
 public class ApplicationUserModel : IdentityUser<Guid>, IUserModel
 {
-    public Guid? ApplicationUserProfileId { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public virtual ICollection<Team>? Teams { get; set; }
     public virtual ICollection<Team>? CreatedTeams { get; set; }   
     public virtual ICollection<Folder>? Folders { get; set; }    
-    // public virtual ICollection<Key>? Keys { get; set; }    
-    // public virtual ICollection<ApplicationUserTeam>? ApplicationUserTeam { get; set; }
 }
